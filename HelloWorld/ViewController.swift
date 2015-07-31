ad996a1d798994964d73459e851d4a81e336b4d0
Bojie Jiang <jiangbojie@gmail.com>
2015-07-31 11:21:37 -0700

    project updated

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,7 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var numberTextFild: UITextField!
+    
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBAction func isPrime(sender: AnyObject) {
+        
+        let number = Int(numberTextFild.text!)!
+        
+        var isPrime = true
+        
+        if number == 1 {
+            isPrime = false
+        }
+        
+        if number != 2 && number != 1 {
+            for var i = 2; i < number; i++ {
+                if number % i == 0 {
+                    isPrime = false
+                    break
+                }
+            }
+        }
+        
+        print(isPrime)
+        
+        if isPrime {
+            
+            resultLabel.text = "\(number) is prime!"
+        } else {
+            resultLabel.text = "\(number) is not prime!"
+        }
+
+        
+    }
+   
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
